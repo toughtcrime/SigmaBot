59ffd2886002602837d11afa9dd1dbcfffaf164e
toughtcrime <vladyslavhlovatskyi@gmail.com>
2023-05-15 14:08:58 +0200

    Added my project

---
+++ SigmaBot/Program.cs
@@ -0,0 +1,77 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+internal class Program
+{
+    static TelegramBotClient botClient = new("here is your key");
+    static async Task Main(string[] args)
+    {
+        using CancellationTokenSource cts = new();
+
+        // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() // receive all update types except ChatMember related updates
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        var me = await botClient.GetMeAsync();
+
+        Console.WriteLine($"Start listening for @{me.Username}");
+        Console.ReadLine();
+
+        // Send cancellation request to stop bot
+        cts.Cancel();
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Only process Message updates: https://core.telegram.org/bots/api#message
+            if (update.Message is not { } message)
+                return;
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+            Random rnd = default;
+            if (messageText.StartsWith("/sigma"))
+            {
+                rnd = new Random();
+                int sigmaRate = rnd.Next(0, 100);
+                // Echo received message text
+                Message sentMessage = await botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Your sigma rate is " + sigmaRate.ToString() + "%",
+                    cancellationToken: cancellationToken);
+                Console.WriteLine($"Received a '{messageText}' | {chatId} | {message.Chat.FirstName} {message.Chat.LastName} sigma rate is {sigmaRate}%");
+                sigmaRate = 0;
+            }
+
+
+
+
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
